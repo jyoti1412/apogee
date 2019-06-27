8ccac1a05be6d153ba9f22b141fba5a945e1db4e
Himanshu Tamrakar <himanshu.tamrakar@decimal.co.in>
2019-06-27 22:44:03 +0530

    1. dashboard component configuration based cards,
    2. Handle data  through *ngFor

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -7,28 +7,121 @@ import * as $ from 'jquery';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  count = 0;
+  public data: any[] = [
+    [{
+      gate: 'GATE 1',
+      title: 'Current In',
+      number: 100,
+      backgroudColor: 'yellow'
+    },
+    {
+      gate: 'GATE 2',
+      title: 'Current Out',
+      number: 200,
+      backgroudColor: 'red'
+    },
+    {
+      gate: 'GATE 3',
+      title: 'Voltage In',
+      number: 300,
+      backgroudColor: 'green'
+    },
+    {
+      gate: 'GATE 4',
+      title: 'Voltage Out',
+      number: 400,
+      backgroudColor: 'black'
+    }],
+
+    [{
+      gate: 'GATE 1',
+      title: 'Current In',
+      number: 100,
+      backgroudColor: 'yellow'
+    },
+    {
+      gate: 'GATE 2',
+      title: 'Current Out',
+      number: 200,
+      backgroudColor: 'red'
+    },
+    {
+      gate: 'GATE 3',
+      title: 'Voltage In',
+      number: 300,
+      backgroudColor: 'green'
+    },
+    {
+      gate: 'GATE 4',
+      title: 'Voltage Out',
+      number: 400,
+      backgroudColor: 'black'
+    }],
+    [{
+      gate: 'GATE 1',
+      title: 'Current In',
+      number: 100,
+      backgroudColor: 'yellow'
+    },
+    {
+      gate: 'GATE 2',
+      title: 'Current Out',
+      number: 200,
+      backgroudColor: 'red'
+    },
+    {
+      gate: 'GATE 3',
+      title: 'Voltage In',
+      number: 300,
+      backgroudColor: 'green'
+    },
+    {
+      gate: 'GATE 4',
+      title: 'Voltage Out',
+      number: 400,
+      backgroudColor: 'black'
+    }]
+
+  ]
+
+
+  public finalData = [
+    {
+      gate: 'GATE 1',
+      title: 'Current In',
+      number: 100,
+      backgroudColor: 'yellow'
+    },
+     {
+      gate: 'GATE 4',
+      title: 'Voltage Out',
+      number: 400,
+      backgroudColor: 'yellow'
+    },
+    {
+     gate: 'GATE 4',
+     title: 'Voltage Out',
+     number: 400,
+     backgroudColor: 'yellow'
+   }
+  ];
 
   constructor() { }
 
   ngOnInit() {
 
-    var flip = 0;
-    $(document).ready(function() {
-    
-         setInterval(function() { 
-            flip=flip+1;
-            $(".flipcard").hide();
-            $("#card"+flip).show();
-            if(flip==4) {
-              flip=0;
-            }
-    
-         }, 1000);
-        });
+    setInterval(() => {
+      debugger
+      ++this.count;
+      if (this.count > 3) this.count = 0;
+
+      [0,1, 2].forEach((i, index) => {
+        this.finalData[i] = this.data[i][this.count];
+      })
+
+    }, 1000)
 
   }
 
-
-
-  
 }
